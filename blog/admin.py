5ab79528c3999361f75ceb8314e6e47b1231e7e5
Ian Ganza <144003894+i-ganza007@users.noreply.github.com>
2025-01-25 22:49:07 +0200

    Add files via upload

---
+++ blog/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import Post
+# If you want your models , and their info to be show up on the admin site , register them here
+admin.site.register(Post)
